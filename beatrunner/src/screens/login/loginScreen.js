4914f26afe82d5c637fa4770ff93eefe157fdac8
Peter Stenger <peter.a.stenger@gmail.com>
2021-02-05 03:06:39 -0500

    convert login to function

--- beatrunner/src/screens/login/loginScreen.js
+++ beatrunner/src/screens/login/loginScreen.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import { View, Button, Alert } from 'react-native'
 
 import authHandler from '_utils/authenticationHandler'
 
-class LoginScreen extends Component {
+function LoginScreen() {
     state = { }
-    render() {
-        return (
-            <View>
-                <Button onPress={() => authHandler.onLogin()} title="Press to login!"/>
-                <Button onPress={() => Alert.alert('button pressed')} title="Create alert"/>
-            </View>
-        )
-    }
+    return (
+        <View>
+            <Button onPress={() => authHandler.onLogin()} title="Press to login!"/>
+            <Button onPress={() => Alert.alert('button pressed')} title="Create alert"/>
+        </View>
+    )
 }
 
 export default LoginScreen;
