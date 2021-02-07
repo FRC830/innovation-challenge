e1ef59b54a9670998400fa7f1f5dc05ef2ed2299
Peter Stenger <peter.a.stenger@gmail.com>
2021-02-07 15:35:08 -0500

    fix build

--- beatrunner/index.js
+++ beatrunner/index.js
@@ -6,4 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
+console.log('Starting...')
 AppRegistry.registerComponent(appName, () => App);
--- beatrunner/src/App.js
+++ beatrunner/src/App.js
@@ -11,10 +11,16 @@ import {
 } from 'react-native'
 
 import { PersistGate } from 'redux-persist/es/integration/react'
+import { createStore } from 'redux'
+import {Provider} from 'react-redux';
 
+import { persistStore } from 'redux-persist'
 import LoginScreen from '_screens/login/LoginScreen'
 
-import { store, persistor } from '_redux/store'
+import rootReducer from '_redux/reducers'
+
+const store = createStore(rootReducer)
+let persistor = persistStore(store)
 
 function App() {
   return (
