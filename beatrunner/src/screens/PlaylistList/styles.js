90b6895d2fd0f937b4fae8be4e28ce29b808c6ea
EliWiegman <ebwiegman@gmail.com>
2021-03-07 16:24:01 -0500

    test push

--- beatrunner/src/screens/PlaylistList/styles.js
+++ beatrunner/src/screens/PlaylistList/styles.js
@@ -3,12 +3,14 @@ import styled from 'styled-components/native'
 export const MyText = styled.Text``
 
 export const MyButton = styled.TouchableOpacity`
-    background: lightblue
-    margin: 10px`
+    background: gray;
+    margin: 15px
+    padding: 50px
+    font-family: serif !important;`
 
 export const MyList = styled.FlatList``
 
 export const MyView = styled.View`
-  background: #233059;
+  background: #ffffff;
   height: 100%;
 `
