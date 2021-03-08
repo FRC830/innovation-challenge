706c2d42bdee1651f4a7f720c06d1d1b41abe3c0
rosebasch <rosebasch22@gmail.com>
2021-03-08 15:07:21 -0500

    pretty logout screen woo!

--- beatrunner/src/screens/Logout/styles.js
+++ beatrunner/src/screens/Logout/styles.js
@@ -1,12 +1,32 @@
 import styled from 'styled-components/native'
 
 export const View = styled.View`
+  background: #233059;
+  height: 100%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  background: #233059;
 `
-export const MyButton = styled.TouchableOpacity`
-  background: lightblue;
+export const LogoutButton = styled.TouchableOpacity`
+  background: #89CFDD;
   padding: 10px;
-  align-items: center;
+  border: 2px;
+  border-color: black;
+  border-radius: 20px;
+  margin-right: 60px;
+  margin-left: 60px;
+  height: 50px
+  width: auto;
+  margin-bottom: 30px;
+  margin-top: 30px;
 `
-export const Text = styled.Text``
+export const NavigateButton = styled(LogoutButton)`
+`
+
+export const Text = styled.Text`
+  color: white;
+  font-family: "Lato";
+  font-size: 20px;
+  text-align: center;
+`
